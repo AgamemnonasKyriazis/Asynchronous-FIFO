5e085c3ffbd465b3bdf709012ef54668ba2ce242
Agamemnnon Kyriazis <agamemnonaskyriazis@gmail.com>
2023-08-14 17:17:41 +0300

    Updated project files and fixed some bugs

--- async_fifo.v
+++ async_fifo.v
@@ -1,3 +1,6 @@
+`include "synchronizer.v"
+`timescale 1ns/1ps
+
 module async_fifo #(
     parameter WIDTH = 32,      // width of data bus
     parameter DEPTH = 16       // depth of FIFO buffer
@@ -9,8 +12,8 @@ module async_fifo #(
     input wire we_i,
     input wire re_i,
     output wire [WIDTH-1:0] rdata_o,
-    output reg full_o,
-    output reg empty_o
+    output wire full_o,
+    output wire empty_o
 );
 
 localparam ADDR_BITS = $clog2(DEPTH);   // address width for buffer
@@ -26,8 +29,6 @@ wire [ADDR_BITS-1:0] w_addr, r_addr;
 wire [ADDR_BITS:0] w_ptr_sync_gray, r_ptr_sync_gray;
 reg [ADDR_BITS:0] w_ptr_sync_bin, r_ptr_sync_bin;
 
-wire full_next, empty_next;
-
 reg [WIDTH-1:0] mem [0:DEPTH-1];
 
 assign w_ptr_next = (we_i & ~full_o)? w_ptr + 1'b1 : w_ptr;
@@ -42,18 +43,18 @@ assign r_ptr_gray = r_ptr ^ (r_ptr >> 1);
 assign w_ptr_next_gray = w_ptr_next ^ (w_ptr_next >> 1);
 assign r_ptr_next_gray = r_ptr_next ^ (r_ptr_next >> 1);
 
-cdc #(
+ff2_sync #(
     .WIDTH(ADDR_BITS+1)
-) cdc_w2r_w_ptr (
+) sync_w2r_w_ptr (
     .clk_i(rclk_i),
     .rst_ni(rst_ni),
     .wdata_i(w_ptr_gray),
     .rdata_o(w_ptr_sync_gray)
 );
 
-cdc #(
+ff2_sync #(
     .WIDTH(ADDR_BITS+1)
-) cdc_r2w_r_ptr (
+) sync_r2w_r_ptr (
     .clk_i(wclk_i),
     .rst_ni(rst_ni),
     .wdata_i(r_ptr_gray),
@@ -77,10 +78,8 @@ always @(posedge wclk_i, negedge rst_ni)
     else
         w_ptr <= w_ptr_next;
 
-always @(posedge wclk_i, negedge rst_ni) begin : write
-    if(~rst_ni)
-        mem[0] <= 0; 
-    else if(we_i & ~full_o)
+always @(posedge wclk_i) begin : write
+    if(we_i & ~full_o)
         mem[w_addr] <= wdata_i;
 end
 
@@ -91,22 +90,8 @@ always @(posedge rclk_i, negedge rst_ni) begin : read
         r_ptr <= r_ptr_next;
 end
 
-always @(posedge wclk_i, negedge rst_ni) begin : full
-    if(~rst_ni)
-        full_o <= 1'b0;
-    else
-        full_o <= full_next;
-end
-
-always @(posedge rclk_i, negedge rst_ni) begin : empty
-    if(~rst_ni)
-        empty_o <= 1'b1;
-    else
-        empty_o <= empty_next;
-end
-
-assign full_next = w_ptr == {~r_ptr_sync_bin[ADDR_BITS], r_ptr_sync_bin[ADDR_BITS-1:0]};
-assign empty_next = r_ptr == w_ptr_sync_bin;
+assign full_o = w_ptr == {~r_ptr_sync_bin[ADDR_BITS], r_ptr_sync_bin[ADDR_BITS-1:0]};
+assign empty_o = r_ptr == w_ptr_sync_bin;
 assign rdata_o = mem[r_addr];
 
 endmodule
